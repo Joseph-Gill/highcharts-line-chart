c7f06caf248d7ae610c7bfc30d9f83d34388b8de
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-10-25 18:30:22 +0100

    Added Highcharts, Highcharts-React-Official, and setup line-chart options

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import HighCharts from 'highcharts';
+import HighchartsReact from "highcharts-react-official";
 import './css/App.css';
 
-function App() {
-  return (
-    <div>Test Goes Here</div>
+const App = () => {
+  const [options, setOptions] = useState(
+    {
+      title: {
+        text: 'Squirro Line Chart Coding Challenge'
+      },
+      subtitle: {
+        text: 'by Joseph Gill'
+      },
+      yAxis: {
+        title: {
+          text: 'USD'
+        }
+      },
+      xAxis: {
+        categories: ['1980', '1985', '1990', '1995', '2000']
+      },
+      series: [
+        {
+          name: 'Sales',
+          data: [14000, 22000, 104000, 87000, 43400]
+        }
+      ]
+    }
   );
-}
+
+  return (
+    <div className="App">
+      <HighchartsReact highcharts={HighCharts} options={options}/>
+    </div>
+  )
+};
 
 export default App;
